ffe1946871417f5955db81e9f24c49ffeee7e600
akunmagang111 <akunmagang111@gmail.com>
2025-09-01 10:31:09 +0700

    test starter v1.2

---
+++ app/Http/Controllers/_superadmin/SuperadminDashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\_superadmin;
+
+use App\Http\Controllers\Controller;
+
+class SuperadminDashboardController extends Controller
+{
+    public function index()   { return view('_superadmin.dashboard.index'); }
+    public function index2()  { return view('_superadmin.dashboard.index2'); }
+    public function index3()  { return view('_superadmin.dashboard.index3'); }
+    public function index4()  { return view('_superadmin.dashboard.index4'); }
+    public function index5()  { return view('_superadmin.dashboard.index5'); }
+    public function index6()  { return view('_superadmin.dashboard.index6'); }
+    public function index7()  { return view('_superadmin.dashboard.index7'); }
+    public function index8()  { return view('_superadmin.dashboard.index8'); }
+    public function index9()  { return view('_superadmin.dashboard.index9'); }
+    public function index10() { return view('_superadmin.dashboard.index10'); }
+}
